1b2d3144a364e24e86f927f0183e32c026e99791
Martin Tazreiter <Martin.Tazreiter@gmx.at>
2021-05-29 19:54:45 +0200

    included power method

--- solveDiffusionEquation.py
+++ solveDiffusionEquation.py
@@ -45,7 +45,12 @@ def CreateIndexArray(lattice):
     i = 0
     for k in range(N_lines):
         for n in range(N_columns):
-            if lattice[k,n] != lead_index:
+            if (lattice[k,n] != lead_index) and lattice[k,n] <= 1400: # fuel assemblies
+                index[k,n] = i
+                i += 1
+    for k in range(N_lines):
+        for n in range(N_columns):
+            if lattice[k,n] != lead_index and lattice[k,n] > 1400:
                 index[k,n] = i
                 i += 1
     return index   
@@ -113,6 +118,63 @@ def GetArrayFromVector(f_vec,index_array):
         f_arr[row,column] = f
     return f_arr
 
+def DirectMethod(A,F,efficient=True,plot_mode_0=True,plot_and_save_all_modes=False):
+    A_inv = np.linalg.inv(A)
+    F_A_inv = F[:,np.newaxis]*A_inv
+    if efficient:
+        k_temp,f_temp = np.linalg.eig(F_A_inv[:19,:19])
+        idx = k_temp.argsort()[::-1]   
+        k = np.zeros(85)
+        f = np.zeros_like(A)
+        k[:19] = k_temp[idx]
+        f[:19,:19] = f_temp[:,idx]
+    else:
+        k_temp,f_temp = np.linalg.eig(F_A_inv)
+        idx = k_temp.argsort()[::-1]   
+        k = k_temp[idx]
+        f = f_temp[:,idx]
+    k[k==0] = np.nan
+    Phi = np.dot(A_inv,f/k)
+    if plot_mode_0 == True:
+        mode = 0
+        f_arr = GetArrayFromVector(f[:,mode],index_array)
+        fig2,ax2 = drawHexagons.PlotHexagons(f_arr,pitch=pitch)
+        ax2.set_title('fission rate / arbitrary units')
+        Phi_arr = GetArrayFromVector(Phi[:,mode],index_array)
+        fig3,ax3 = drawHexagons.PlotHexagons(Phi_arr,pitch=pitch)
+        ax3.set_title('Phi / arbitrary units')
+    if plot_and_save_all_modes:
+        for i in range(19):
+                plt.close('all')
+                f_arr = GetArrayFromVector(f[:,i],index_array)
+                fig2,ax2 = drawHexagons.PlotHexagons(f_arr,pitch=pitch,draw_axis=False,draw_colorbar=False,L = index_array!=-1)
+                fig2.savefig('fission_source{}.eps'.format(i))
+                Phi_arr = GetArrayFromVector(Phi[:,i],index_array)
+                fig3,ax3 = drawHexagons.PlotHexagons(Phi_arr,pitch=pitch,draw_axis=False,draw_colorbar=False,L = index_array!=-1)
+                fig3.savefig('phi{}.eps'.format(i))
+                print(r'\centered{' + '{:.5f}'.format(k[2*i]) + r'} & \centered{\includegraphics[width=0.15\linewidth]{pictures/fission_source' + str(2*i) + r'.eps}} & \centered{\includegraphics[width=0.15\linewidth]{pictures/phi' + str(2*i) + r'.eps}} &',end='')
+                print(r'\centered{' + '{:.5f}'.format(k[2*i+1]) + r'} & \centered{\includegraphics[width=0.15\linewidth]{pictures/fission_source' + str(2*i+1) + r'.eps}} & \centered{\includegraphics[width=0.15\linewidth]{pictures/phi' + str(2*i+1) + r'.eps}} \\')
+        print(r'\centered{' + '{:.5f}'.format(k[18]) + r'} & \centered{\includegraphics[width=0.15\linewidth]{pictures/fission_source' + str(18) + r'.eps}} & \centered{\includegraphics[width=0.15\linewidth]{pictures/phi' + str(18) + r'.eps}} & & \\')
+    return k
+    
+def PowerMethod(A,F,plot=True):
+    N = 60
+    k_tilde = np.ones(N+1)
+    f = np.ones(len(F))
+    for i in range(N):
+        Phi = np.linalg.solve(A,f/k_tilde[i])
+        f_new = F*Phi
+        k_tilde[i+1] = k_tilde[i]*np.dot(f_new,f)/np.dot(f,f)
+        f = f_new
+    if plot == True:
+        f_arr = GetArrayFromVector(f,index_array)
+        fig2,ax2 = drawHexagons.PlotHexagons(f_arr,pitch=pitch)
+        ax2.set_title('fission rate / arbitrary units')
+        Phi_arr = GetArrayFromVector(Phi,index_array)
+        fig3,ax3 = drawHexagons.PlotHexagons(Phi_arr,pitch=pitch)
+        ax3.set_title('Phi / arbitrary units')
+    return k_tilde, f
+
 if __name__ == "__main__":
     plt.close('all')
     t1 = time.time()
@@ -142,31 +204,18 @@ if __name__ == "__main__":
 
     A = CreateMatrixA(index_array,gc_dict,lattice,pitch)
     F = GetVectorF(index_array,gc_dict,lattice,pitch)   
-    A_inv = np.linalg.inv(A)
-    F_A_inv = F[:,np.newaxis]*A_inv
     
-    k_temp,f_temp = np.linalg.eig(F_A_inv)
-    idx = k_temp.argsort()[::-1]   
-    k = k_temp[idx]
-    f = f_temp[:,idx]
+    k_exact = DirectMethod(A,F,plot_mode_0=True,plot_and_save_all_modes=False)
+    k,f = PowerMethod(A,F,plot=False)
     
-    Phi = np.dot(A_inv,f/k)
-    # f_arr = GetArrayFromVector(f[:,1],index_array)
-    # fig2,ax2 = drawHexagons.PlotHexagons(f_arr,pitch=pitch)
-    # ax2.set_title('fission rate / arbitrary units')
-    # Phi_arr = GetArrayFromVector(Phi[:,1],index_array)
-    # fig3,ax3 = drawHexagons.PlotHexagons(Phi_arr,pitch=pitch)
-    # ax3.set_title('Phi / arbitrary units')
-    for i in range(7,9):
-            # plt.close('all')
-            # f_arr = GetArrayFromVector(f[:,i],index_array)
-            # fig2,ax2 = drawHexagons.PlotHexagons(f_arr,pitch=pitch,draw_axis=False,draw_colorbar=False,L = index_array!=-1)
-            # fig2.savefig('fission_source{}.eps'.format(i))
-            # Phi_arr = GetArrayFromVector(Phi[:,i],index_array)
-            # fig3,ax3 = drawHexagons.PlotHexagons(Phi_arr,pitch=pitch,draw_axis=False,draw_colorbar=False,L = index_array!=-1)
-            # fig3.savefig('phi{}.eps'.format(i))
-            print(r'\centered{' + '{:.5f}'.format(k[2*i]) + r'} & \centered{\includegraphics[width=0.15\linewidth]{pictures/fission_source' + str(2*i) + r'.eps}} & \centered{\includegraphics[width=0.15\linewidth]{pictures/phi' + str(2*i) + r'.eps}} &',end='')
-            print(r'\centered{' + '{:.5f}'.format(k[2*i+1]) + r'} & \centered{\includegraphics[width=0.15\linewidth]{pictures/fission_source' + str(2*i+1) + r'.eps}} & \centered{\includegraphics[width=0.15\linewidth]{pictures/phi' + str(2*i+1) + r'.eps}} \\')
-    print(r'\centered{' + '{:.5f}'.format(k[18]) + r'} & \centered{\includegraphics[width=0.15\linewidth]{pictures/fission_source' + str(18) + r'.eps}} & \centered{\includegraphics[width=0.15\linewidth]{pictures/phi' + str(18) + r'.eps}} & & \\')
-            
+    fig,ax = plt.subplots()
+    n = np.arange(len(k))
+    ax.semilogy(n,np.abs(k-k_exact[0]),label='power method')
+    ax.semilogy(n,(k_exact[1]/k_exact[0])**(2*n),label=r'$\left(\frac{k_1}{k_0}\right)^{2n}$')
+    ax.semilogy(n,(k_exact[1]/k_exact[0])**(n),label=r'$\left(\frac{k_1}{k_0}\right)^{n}$')
+
+    ax.set_ylabel(r'$\tilde{k}_n - k_0$')
+    ax.set_xlabel('number of iterations = n')
+    ax.grid()
+    ax.legend()
     print('time = {:.4f} s'.format(time.time()-t1))
